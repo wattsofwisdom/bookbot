4d6594843422d1a532c23bfc3d11b9e772251b3d
wattsofwisdom <hooveraaron14@gmail.com>
2024-10-13 21:35:13 -0400

    add word count function

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+def count_words(content):
+    """Count the words in content"""
+    words = content.split()
+    word_count = len(words)
+    print(word_count)
+
 # Open the file in read mode
 book = open("books/frankenstein.txt","r")
 
 # read the entire content of the file and print
 content = book.read()
-print(content)
+# print(content)
+count_words(content)
 
 # close the file
 book.close()
+
