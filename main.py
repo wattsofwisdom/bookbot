b998299f5a515345bfcd73267dd933472ef437c4
wattsofwisdom <hooveraaron14@gmail.com>
2024-10-30 14:00:12 -0400

    add report printing

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    print(f"--- Begin report of {book_path} ---")
+
+    # Open the file in read mode
+    book = open(book_path,"r")
+
+    # read the entire content of the file and print
+    content = book.read()
+
+    # close the file
+    book.close()
+
+    # get total word count
+    tot_num_words = count_words(content)
+
+    # get character count
+    chars_dict = count_chars(content.lower())
+
+    print(f"{tot_num_words} words found in the document\n")
+
+    for key, value in chars_dict.items():
+        if not key.isalpha():
+            continue
+        print(f"The '{key}' character was found {value} times")
+    print("--- End of Report ---")
+
 def count_words(content):
     """Count the words in content"""
     words = content.split()
     word_count = len(words)
-    print(word_count)
+    return word_count
 
 def count_chars(content):
     """Count the number of characters"""
@@ -14,15 +41,4 @@ def count_chars(content):
             cnt[c] = 1
     return cnt
 
-# Open the file in read mode
-book = open("books/frankenstein.txt","r")
-
-# read the entire content of the file and print
-content = book.read()
-# print(content)
-count_words(content)
-print(count_chars(content.lower()))
-
-# close the file
-book.close()
-
+main()
